2dfd0f359446589d5a1390d6fd299ac1d75381e1
Aigio Liu <lysoft.pw@foxmail.com>
2021-03-24 19:46:59 +0800

    fix appcenter

--- src/ST.Client.Desktop.Windows/Application/VisualStudioAppCenterSDK.cs
+++ src/ST.Client.Desktop.Windows/Application/VisualStudioAppCenterSDK.cs
@@ -1,11 +1,13 @@
 ﻿using Microsoft.AppCenter;
 using Microsoft.AppCenter.Analytics;
 using Microsoft.AppCenter.Crashes;
+using System.Runtime.Versioning;
 
 namespace System.Application
 {
     public static class VisualStudioAppCenterSDK
     {
+        [SupportedOSPlatform("Windows")]
         public static void Init(string appSecret)
         {
             AppCenter.Start(appSecret, typeof(Analytics), typeof(Crashes));
--- src/ST.Client.Mobile.iOS.App/Main.cs
+++ src/ST.Client.Mobile.iOS.App/Main.cs
@@ -2,7 +2,7 @@
 
 namespace System.Application
 {
-    static class Program
+    public class Application
     {
         // This is the main entry point of the application.
         static void Main(string[] args)
