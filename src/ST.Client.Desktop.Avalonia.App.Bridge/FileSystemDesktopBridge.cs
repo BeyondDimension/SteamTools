64346f47ad1caf236b348b55af340d8d65ec5123
Aigio Liu <lysoft.pw@foxmail.com>
2021-05-26 19:54:59 +0800

    DesktopBridge

--- src/ST.Client.Desktop.Avalonia.App.Bridge/FileSystemDesktopBridge.cs
+++ src/ST.Client.Desktop.Avalonia.App.Bridge/FileSystemDesktopBridge.cs
@@ -10,8 +10,8 @@ namespace System.Application
             // https://github.com/xamarin/Essentials/blob/1.6.1/Xamarin.Essentials/FileSystem/FileSystem.uwp.cs#L12
             // https://docs.microsoft.com/zh-cn/windows/msix/desktop/desktop-to-uwp-behind-the-scenes
             // 不允许在 C:\Program Files\WindowsApps\package_name 下写入。
-            // C:\Users\{user}\AppData\Local\Packages\net.steampp.app_gvn1zxq6vcwjj\LocalState
-            // C:\Users\{user}\AppData\Local\Packages\net.steampp.app_gvn1zxq6vcwjj\LocalCache
+            // C:\Users\{user}\AppData\Local\Packages\4651ED44255E.47979655102CE_gvn1zxq6vcwjj\LocalState
+            // C:\Users\{user}\AppData\Local\Packages\4651ED44255E.47979655102CE_gvn1zxq6vcwjj\LocalCache
             IOPath.InitFileSystem(() => ApplicationData.Current.LocalFolder.Path, () => ApplicationData.Current.LocalCacheFolder.Path);
         }
     }
