90f1aae4bdc383820624b932ad5738c5bd6bec1b
Aigio Liu <lysoft.pw@foxmail.com>
2021-06-11 22:30:21 +0800

    Android Native UI

--- src/ST.Client.Mobile.Droid.Design/app/src/main/java/net/steampp/app/design/ui/fragments/MyFragment.java
+++ src/ST.Client.Mobile.Droid.Design/app/src/main/java/net/steampp/app/design/ui/fragments/MyFragment.java
@@ -28,13 +28,13 @@ public class MyFragment extends Fragment {
         binding = FragmentMyBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textView;
-        viewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+//        final TextView textView = binding.textView;
+//        viewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
         return root;
     }
 
