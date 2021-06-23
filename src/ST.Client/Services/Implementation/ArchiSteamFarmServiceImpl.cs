2559fc2c5da3f7e6e99d814be17ccabc8cdd6a4a
rmbgame <rmb@rmbgame.net>
2021-06-24 00:25:26 +0800

    CI Build Error Fix

---
+++ src/ST.Client/Services/Implementation/ArchiSteamFarmServiceImpl.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System.Application.Services.Implementation
+{
+    public class ArchiSteamFarmServiceImpl
+    {
+        public void Init() 
+        {
+
+        }
+
+        public void Start()
+        {
+
+            await Init(args).ConfigureAwait(false);
+        }
+    }
+}
