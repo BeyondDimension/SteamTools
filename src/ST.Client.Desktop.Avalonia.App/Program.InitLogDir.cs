42c0842a1da254c70f91ccb5dc3730b861a875be
Aigio Liu <lysoft.pw@foxmail.com>
2021-06-30 00:44:55 +0800

    ASF Log Config

--- src/ST.Client.Desktop.Avalonia.App/Program.InitLogDir.cs
+++ src/ST.Client.Desktop.Avalonia.App/Program.InitLogDir.cs
@@ -1,3 +1,4 @@
+using ArchiSteamFarm;
 using NLog;
 using NLog.Common;
 using NLog.Config;
@@ -19,7 +20,7 @@ namespace System.Application.UI
         public static string InitLogDir(string? alias = null)
         {
             var logDirPath = Path.Combine(
-#if WINDOWS_DESKTOP_BRIDGE
+#if WINDOWS_DESKTOP_BRIDGE || MAC
                 IOPath.CacheDirectory
 #else
                 IOPath.BaseDirectory
@@ -34,6 +35,7 @@ namespace System.Application.UI
             InternalLogger.LogFile = logDirPath_ + "internal-nlog" + alias + ".txt";
             InternalLogger.LogLevel = NLogLevel.Error;
             var objConfig = new LoggingConfiguration();
+            var defMinLevel = AppHelper.DefaultNLoggerMinLevel;
             var logfile = new FileTarget("logfile")
             {
                 FileName = logDirPath_ + "nlog-all-${shortdate}" + alias + ".log",
@@ -42,10 +44,29 @@ namespace System.Application.UI
                 MaxArchiveFiles = 14,
                 MaxArchiveDays = 7,
             };
+            var asfLogDirPath = ASFPathHelper.GetLogDirectory(logDirPath_);
+            FileTarget logfile_asf = new("File")
+            {
+                ArchiveFileName = ASFPathHelper.GetNLogArchiveFileName(asfLogDirPath),
+                ArchiveNumbering = ArchiveNumberingMode.Rolling,
+                ArchiveOldFileOnStartup = true,
+                CleanupFileName = false,
+                ConcurrentWrites = false,
+                DeleteOldFileOnStartup = true,
+                FileName = ASFPathHelper.GetNLogFileName(asfLogDirPath),
+                Layout = ASFPathHelper.NLogGeneralLayout,
+                ArchiveAboveSize = 10485760,
+                MaxArchiveFiles = 10,
+                MaxArchiveDays = 7,
+            };
             objConfig.AddTarget(logfile);
+            objConfig.AddTarget(logfile_asf);
+
             objConfig.AddRule(NLogLevel.Error, NLogLevel.Fatal, logfile, "Microsoft.*");
             objConfig.AddRule(NLogLevel.Error, NLogLevel.Fatal, logfile, "System.Net.Http.*");
-            objConfig.AddRule(AppHelper.DefaultNLoggerMinLevel, NLogLevel.Fatal, logfile, "*");
+            objConfig.LoggingRules.Add(new LoggingRule("ArchiSteamFarm*", NLogLevel.Off, logfile) { Final = true, });
+            objConfig.AddRule(defMinLevel, NLogLevel.Fatal, logfile, "*");
+            objConfig.LoggingRules.Add(new LoggingRule("ArchiSteamFarm*", defMinLevel, logfile_asf));
 #if StartupTrace
             StartupTrace.Restart("InitLogDir.CreateLoggingConfiguration");
 #endif
