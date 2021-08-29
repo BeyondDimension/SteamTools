dbd5512dcb7dd877b931f34ae36b8449e29761a9
Aigio Liu <lysoft.pw@foxmail.com>
2021-08-29 22:09:45 +0800

    fix

--- src/ST.Client.Desktop.Avalonia/Application/Services/Implementation/PlatformNotificationServiceImpl.cs
+++ src/ST.Client.Desktop.Avalonia/Application/Services/Implementation/PlatformNotificationServiceImpl.cs
@@ -1,28 +1,36 @@
 using System.Application.UI;
 using System.Properties;
+using System.Windows;
 
 namespace System.Application.Services.Implementation
 {
     /// <inheritdoc cref="INotificationService{TNotificationType, TEntrance}"/>
     internal sealed class PlatformNotificationServiceImpl : INotificationService
     {
+        readonly NotifyIcon notifyIcon;
+
+        public PlatformNotificationServiceImpl(NotifyIcon notifyIcon)
+        {
+            this.notifyIcon = notifyIcon;
+        }
+
         bool INotificationService<NotificationType, Entrance, INotificationService>.AreNotificationsEnabled() => true;
 
         void INotificationService<NotificationType, Entrance, INotificationService>.Cancel(NotificationType notificationType)
         {
-            INotifyIcon.Instance.HideBalloonTip();
+            notifyIcon.HideBalloonTip();
         }
 
         void INotificationService<NotificationType, Entrance, INotificationService>.CancelAll()
         {
-            INotifyIcon.Instance.HideBalloonTip();
+            notifyIcon.HideBalloonTip();
         }
 
         void INotificationService<NotificationType, Entrance, INotificationService>.Notify(string text, NotificationType notificationType, bool autoCancel, string? title, Entrance entrance)
         {
             title ??= ThisAssembly.AssemblyTrademark;
             // 调用托盘显示通知
-            INotifyIcon.Instance.ShowBalloonTip(title, text);
+            notifyIcon.ShowBalloonTip(title, text, ToolTipIcon.None);
         }
 
         Progress<float> INotificationService<NotificationType, Entrance, INotificationService>.NotifyDownload(Func<string> text, NotificationType notificationType, string? title)
