8303b96284c482265040bc6cf933680b28566989
Aigio Liu <lysoft.pw@foxmail.com>
2021-09-13 17:15:31 +0800

    fix

--- src/ST.Client.Desktop/Services/Implementation/HostsFileServiceImpl.cs
+++ src/ST.Client.Desktop/Services/Implementation/HostsFileServiceImpl.cs
@@ -107,7 +107,7 @@ namespace System.Application.Services.Implementation
                     }
                 }
 
-                if (OperatingSystem2.IsMacOS || (OperatingSystem2.IsLinux && s.IsAdministrator))
+                if (OperatingSystem2.IsMacOS || (OperatingSystem2.IsLinux && !s.IsAdministrator))
                 {
                     var editPath = Path.Combine(IOPath.CacheDirectory, "hosts");
                     var editInfo = new FileInfo(editPath);
