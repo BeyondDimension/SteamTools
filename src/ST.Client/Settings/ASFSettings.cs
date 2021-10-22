f9c97a7938479885d3e62cd62950a541532e7cfe
Aigio Liu <lysoft.pw@foxmail.com>
2021-10-22 15:22:14 +0800

    Add ASFSettings.ConsoleMaxLine

--- src/ST.Client/Settings/ASFSettings.cs
+++ src/ST.Client/Settings/ASFSettings.cs
@@ -23,5 +23,43 @@ namespace System.Application.Settings
         /// </summary>
         public static SerializableProperty<bool> AutoRunArchiSteamFarm { get; }
             = GetProperty(defaultValue: false, autoSave: true);
+
+        #region ConsoleMaxLine
+
+        /// <summary>
+        /// 控制台默认最大行数
+        /// </summary>
+        public const int DefaultConsoleMaxLine = 200;
+
+        /// <summary>
+        /// 控制台默认最大行数范围最小值
+        /// </summary>
+        public const int MinRangeConsoleMaxLine = DefaultConsoleMaxLine;
+
+        /// <summary>
+        /// 控制台默认最大行数范围最大值
+        /// </summary>
+        public const int MaxRangeConsoleMaxLine = 5000;
+
+        /// <summary>
+        /// 获取控制台最大行数
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static int ConsoleMaxLineValue
+        {
+            get
+            {
+                var value = ConsoleMaxLine.Value;
+                if (value < MinRangeConsoleMaxLine) return MinRangeConsoleMaxLine;
+                if (value > MaxRangeConsoleMaxLine) return MaxRangeConsoleMaxLine;
+                return value;
+            }
+        }
+
+        public static SerializableProperty<int> ConsoleMaxLine { get; }
+            = GetProperty(defaultValue: DefaultConsoleMaxLine, autoSave: true);
+
+        #endregion
     }
 }
