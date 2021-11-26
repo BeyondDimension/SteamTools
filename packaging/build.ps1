369da3d830c37a3f787797cb768c314bad7ef860
RMBGAME <rmb@rmbgame.net>
2021-11-27 00:09:00 +0800

    build.ps1

--- packaging/build.ps1
+++ packaging/build.ps1
@@ -30,7 +30,8 @@ function Build-PublishTool
     $Cred = Get-Credential
     $Url = "https://steampp.net"
     $Body = @{
-        key = "search index=_internal | reverse | table index,host,source,sourcetype,_raw"
+        key = "xxxx"
+        version = $version
         output_mode = "pfx"
     }
 #1. 读取服务器接口获取rsa公钥
@@ -41,11 +42,15 @@ function Build-PublishTool
 
 #3. (本地)手动在VS中发布任意一个或多个平台配置(pubxml)，后续可改成命令行自动发布
     Build-App win-x64
-
+    Build-App osx-x64
+    Build-App linux-x64
+    Build-App linux-arm64
     & $publishtool_exe hostpath
 #4. 读取上一步操作后的 Publish.json 生成压缩包并计算哈希值写入 Publish.json
     & $publishtool_exe 7z
-#5. (本地)将 Publish.json 上传至云端
+
+#5. (本地)验证发布文件夹与统计文件
+    & $publishtool_exe sta -val "win-x64" "osx-x64" "linux-x64" "linux-arm64" 
 
     if ($LASTEXITCODE) { exit $LASTEXITCODE }
 }
@@ -67,7 +72,12 @@ function Build-App
 
     Remove-Item $publishDir -Recurse -Force -Confirm:$false -ErrorAction Ignore
 
-    dotnet publish $proj_path -c $configuration /p:PublishProfile=$build_pubxml_dir\$build_pubxml_winx64
+    if($rid -eq 'win-x64'){ $pubxml = "$build_pubxml_dir\$build_pubxml_winx64" }
+    if($rid -eq 'osx-64'){ $pubxml = "$build_pubxml_dir\$build_pubxml_osxx64" }
+    if($rid -eq 'linux-x64'){ $pubxml = "$build_pubxml_dir\$build_pubxml_linuxx64" }
+    if($rid -eq 'linux-arm64'){ $pubxml = "$build_pubxml_dir\$build_pubxml_linuxarm64" }
+
+    dotnet publish $proj_path -c $configuration /p:PublishProfile=$pubxml
 
     if ($LASTEXITCODE) { exit $LASTEXITCODE }
 }
