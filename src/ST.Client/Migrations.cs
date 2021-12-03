f80ec7c79a79cc1bec532d3dbe513d6c39080998
Aigio Liu <lysoft.pw@foxmail.com>
2021-12-03 22:40:26 +0800

    fix

--- src/ST.Client/Migrations.cs
+++ src/ST.Client/Migrations.cs
@@ -16,10 +16,13 @@ namespace System.Application
         /// </summary>
         public static void Up()
         {
-            if (PreviousVersion < new Version(2, 6, 2)) // 上一次运行的版本小于 2.6.2 时将执行以下迁移
+            if (VersionTracking2.IsFirstLaunchForCurrentVersion) // 当前版本号第一次启动
             {
-                // 可以删除 /AppData/application2.dbf 表 0984415E 使此 if 返回 True
-                // 目前此数据库中仅有这一张表，所以可以直接删除文件，但之后可能会新增新表
+                if (PreviousVersion < new Version(2, 6, 2)) // 上一次运行的版本小于 2.6.2 时将执行以下迁移
+                {
+                    // 可以删除 /AppData/application2.dbf 表 0984415E 使此 if 返回 True
+                    // 目前此数据库中仅有这一张表，所以可以直接删除文件，但之后可能会新增新表
+                }
             }
         }
     }
