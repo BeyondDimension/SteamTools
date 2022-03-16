588d663216be943cc2b41e4a154000aba0374bf8
Aigio Liu <lysoft.pw@foxmail.com>
2022-03-16 11:53:46 +0800

    fix osx-arm64.pubxml

--- tests/Common.UnitTest.Droid.App/Resources/Resource.Designer.cs
+++ tests/Common.UnitTest.Droid.App/Resources/Resource.Designer.cs
@@ -14,7 +14,7 @@ namespace System.UnitTest
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.1.0.11")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.2.0.155")]
 	public partial class Resource
 	{
 		
