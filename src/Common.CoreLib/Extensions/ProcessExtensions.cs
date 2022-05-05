5c2fca9db09d56148bef4567a0a7aa4f34d3f9c2
Aigio Liu <lysoft.pw@foxmail.com>
2022-05-05 18:09:02 +0800

    Add KillEntireProcessTree

--- src/Common.CoreLib/Extensions/ProcessExtensions.cs
+++ src/Common.CoreLib/Extensions/ProcessExtensions.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using System.Runtime.Versioning;
 
 // ReSharper disable once CheckNamespace
 namespace System
@@ -15,6 +16,21 @@ namespace System
             {
                 return null;
             }
+        }
+
+        /// <summary>
+        /// 立即停止关联的进程，并停止其子/后代进程。
+        /// </summary>
+        /// <param name="process"></param>
+        [UnsupportedOSPlatform("ios")]
+        [UnsupportedOSPlatform("tvos")]
+        public static void KillEntireProcessTree(this Process process)
+        {
+#if NETCOREAPP3_0_OR_GREATER
+            process.Kill(entireProcessTree: true);
+#else
+            process.Kill();
+#endif
         }
     }
 }
