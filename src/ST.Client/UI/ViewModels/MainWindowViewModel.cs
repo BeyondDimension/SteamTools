d9030f2223cf2d74c139c7cd94fcb8a1f7aeb8b0
RMBGAME <rmb@rmbgame.net>
2022-05-20 12:48:04 +0800

    Misc

--- src/ST.Client/UI/ViewModels/MainWindowViewModel.cs
+++ src/ST.Client/UI/ViewModels/MainWindowViewModel.cs
@@ -96,7 +96,7 @@ namespace System.Application.UI.ViewModels
 
             #region InitTabItems
 
-            AddTabItem<StartPageViewModel>();
+            //AddTabItem<StartPageViewModel>();
             AddTabItem<CommunityProxyPageViewModel>();
             if (IApplication.IsDesktopPlatform)
             {
