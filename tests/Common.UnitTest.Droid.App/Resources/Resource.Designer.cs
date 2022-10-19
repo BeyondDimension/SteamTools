0876cf6dedc15a7c5a390498ce2c45a30a9eca19
RMBGAME <rmbgame@outlook.com>
2022-10-19 22:49:56 +0800

    Misc

--- tests/Common.UnitTest.Droid.App/Resources/Resource.Designer.cs
+++ tests/Common.UnitTest.Droid.App/Resources/Resource.Designer.cs
@@ -14,7 +14,7 @@ namespace System.UnitTest
 {
 	
 	
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "12.3.3.31")]
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.0.0.73")]
 	public partial class Resource
 	{
 		
