f9542f87b713112ef690e038f369678af7d257c1
RMBGAME <rmbgame@outlook.com>
2022-11-11 20:20:12 +0800

    Misc

---
+++ src/ST.Client/Services/Implementation/DnsDohAnalysisService.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net;
+using System.Net.Http;
+
+namespace System.Application.Services;
+
+public sealed class DnsDohAnalysisService : IDnsDohAnalysisService
+{
+    public DnsDohAnalysisService(IHttpService httpService)
+    {
+
+    }
+
+    /// <summary>
+    /// 解析域名 IP 地址
+    /// </summary>
+    /// <param name="hostNameOrAddress">要解析的主机名或 IP 地址</param>
+    /// <param name="isIPv6"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public IAsyncEnumerable<IPAddress> AnalysisDomainIpAsync(string hostNameOrAddress, bool isIPv6, CancellationToken cancellationToken = default)
+        => AnalysisDomainIpAsync(hostNameOrAddress, default, isIPv6, cancellationToken);
+
+    /// <summary>
+    /// 解析域名 IP 地址
+    /// </summary>
+    /// <param name="hostNameOrAddress">要解析的主机名或 IP 地址</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public IAsyncEnumerable<IPAddress> AnalysisDomainIpAsync(string hostNameOrAddress, CancellationToken cancellationToken = default)
+        => AnalysisDomainIpAsync(hostNameOrAddress, default, default, cancellationToken);
+
+    /// <summary>
+    /// 解析域名 IP 地址
+    /// </summary>
+    /// <param name="hostNameOrAddress">要解析的主机名或 IP 地址</param>
+    /// <param name="dnsServers">自定义 DNS 服务器，可选的值有 <see cref="DNS_Alis"/>, <see cref="DNS_114s"/>, <see cref="DNS_Cloudflares"/>, <see cref="DNS_Dnspods"/>, <see cref="DNS_Googles"/></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public IAsyncEnumerable<IPAddress> AnalysisDomainIpAsync(string hostNameOrAddress, string? dnsServers, CancellationToken cancellationToken = default)
+        => AnalysisDomainIpAsync(hostNameOrAddress, dnsServers, default, cancellationToken);
+
+    /// <summary>
+    /// 解析域名 IP 地址
+    /// </summary>
+    /// <param name="hostNameOrAddress">要解析的主机名或 IP 地址</param>
+    /// <param name="dnsServers">自定义 DNS 服务器，可选的值有 <see cref="DNS_Alis"/>, <see cref="DNS_114s"/>, <see cref="DNS_Cloudflares"/>, <see cref="DNS_Dnspods"/>, <see cref="DNS_Googles"/></param>
+    /// <param name="isIPv6"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    public async IAsyncEnumerable<IPAddress> AnalysisDomainIpAsync(string hostNameOrAddress, string? dnsServers, bool isIPv6, CancellationToken cancellationToken = default)
+    {
+        await Task.CompletedTask;
+        yield return IPAddress.Any;
+    }
+}
