a761e56758a0159203dbb58a59d0999903752067
rmbagme <rmb@rmbgame.net>
2023-03-10 19:12:58 +0800

    UI

--- src/BD.WTTS.Client.Avalonia.App/Properties/AssemblyInfo.Xaml.cs
+++ src/BD.WTTS.Client.Avalonia.App/Properties/AssemblyInfo.Xaml.cs
@@ -1,4 +1,10 @@
+//steampp
 [assembly: XmlnsDefinition("https://steampp.net/ui", "BD.WTTS.UI.Views")]
 [assembly: XmlnsDefinition("https://steampp.net/ui", "BD.WTTS.UI.Views.Controls")]
 [assembly: XmlnsDefinition("https://steampp.net/ui", "BD.WTTS.UI.Views.Pages")]
 [assembly: XmlnsDefinition("https://steampp.net/ui", "BD.WTTS.UI.Views.Windows")]
+
+//FluentAvaloniaUI
+[assembly: XmlnsDefinition("https://github.com/amwx/FluentAvalonia", "FluentAvalonia.UI.Controls")]
+[assembly: XmlnsDefinition("https://github.com/amwx/FluentAvalonia", "FluentAvalonia.UI.Windowing")]
+[assembly: XmlnsDefinition("https://github.com/amwx/FluentAvalonia", "FluentAvalonia.UI.Controls.Primitives")]
--- src/BD.WTTS.Client.Avalonia.App/UI/Views/Controls/Class1.cs
+++ src/BD.WTTS.Client.Avalonia.App/UI/Views/Controls/Class1.cs
@@ -1,12 +1,10 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BD.WTTS.UI.Views.Controls
+namespace BD.WTTS.UI.Views.Controls;
+class Class1
 {
-    class Class1
-    {
-    }
 }
