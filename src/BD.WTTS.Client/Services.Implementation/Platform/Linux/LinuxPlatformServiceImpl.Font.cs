23ac963542c93ba24e4afef1d2f68dd61ff42e70
Aigio Liu <lysoft.pw@foxmail.com>
2023-03-18 02:20:35 +0800

    Update Linux Font List

--- src/BD.WTTS.Client/Services.Implementation/Platform/Linux/LinuxPlatformServiceImpl.Font.cs
+++ src/BD.WTTS.Client/Services.Implementation/Platform/Linux/LinuxPlatformServiceImpl.Font.cs
@@ -6,31 +6,63 @@ partial class LinuxPlatformServiceImpl
 {
     string IPlatformService.GetDefaultFontFamily(FontWeight fontWeight)
     {
+        // ubuntu-22.04.2-desktop-amd64.iso
+        // Noto Sans CJK JP
+        // Noto Sans CJK JP Black
+        // Noto Sans CJK KR
+        // Noto Sans CJK KR Black
+        // Noto Sans CJK SC
+        // Noto Sans CJK SC Black
+        // Noto Sans CJK TC
+        // Noto Sans CJK TC Black
+        // Noto Sans CJK HK
+        // Noto Sans CJK HK Black
+        // Noto Sans CJK JP DemiLight
+        // Noto Sans CJK KR DemiLight
+        // Noto Sans CJK SC DemiLight
+        // Noto Sans CJK TC DemiLight
+        // Noto Sans CJK HK DemiLight
+        // Noto Sans CJK JP Light
+        // Noto Sans CJK KR Light
+        // Noto Sans CJK SC Light
+        // Noto Sans CJK TC Light
+        // Noto Sans CJK HK Light
+        // Noto Sans CJK JP Medium
+        // Noto Sans CJK KR Medium
+        // Noto Sans CJK SC Medium
+        // Noto Sans CJK TC Medium
+        // Noto Sans CJK HK Medium
+        // Noto Sans CJK JP Thin
+        // Noto Sans CJK KR Thin
+        // Noto Sans CJK SC Thin
+        // Noto Sans CJK TC Thin
+        // Noto Sans CJK HK Thin
+
         const ushort Thin = 100;
         const ushort ExtraLight = 200;
         const ushort Light = 300;
         const ushort Normal = 400;
         const ushort Medium = 500;
-        const ushort SemiBold = 600;
-        const ushort ExtraBold = 800;
         const ushort Black = 900;
 
         var fontWeightValue = (ushort)fontWeight;
         if (fontWeightValue < 50)
             fontWeightValue = Thin;
-        if (fontWeightValue < 250)
+        else if (fontWeightValue < 250)
             fontWeightValue = ExtraLight;
-        if (fontWeightValue < 350)
+        else if (fontWeightValue < 350)
             fontWeightValue = Light;
-        if (fontWeightValue < 450)
+        else if (fontWeightValue < 450)
             fontWeightValue = Normal;
-        if (fontWeightValue < 550)
+        else if (fontWeightValue < 550)
             fontWeightValue = Medium;
-        if (fontWeightValue < 650)
-            fontWeightValue = SemiBold;
-        if (fontWeightValue < 850)
-            fontWeightValue = ExtraBold;
-        if (fontWeightValue >= 950)
+        //else if (fontWeightValue < 650)
+        //    fontWeightValue = SemiBold;
+        //else if (fontWeightValue < 750)
+        //    fontWeightValue = SemiBold;
+        //else if (fontWeightValue < 850)
+        //    fontWeightValue = ExtraBold;
+        else /*if (fontWeightValue >= 950)*/
             fontWeightValue = Black;
 
         var culture = ResourceService.Culture;
@@ -39,10 +71,9 @@ partial class LinuxPlatformServiceImpl
             return fontWeightValue switch
             {
                 Thin => "Noto Sans CJK SC Thin",
-                ExtraLight => "Noto Sans CJK SC ExtraLight",
+                ExtraLight => "Noto Sans CJK SC DemiLight",
                 Light => "Noto Sans CJK SC Light",
                 Medium => "Noto Sans CJK SC Medium",
-                SemiBold => "Noto Sans CJK SC SemiBold",
                 Black => "Noto Sans CJK SC Black",
                 _ or Normal => "Noto Sans CJK SC",
             };
@@ -52,10 +83,9 @@ partial class LinuxPlatformServiceImpl
             return fontWeightValue switch
             {
                 Thin => "Noto Sans CJK HK Thin",
-                ExtraLight => "Noto Sans CJK HK ExtraLight",
+                ExtraLight => "Noto Sans CJK HK DemiLight",
                 Light => "Noto Sans CJK HK Light",
                 Medium => "Noto Sans CJK HK Medium",
-                SemiBold => "Noto Sans CJK HK SemiBold",
                 Black => "Noto Sans CJK HK Black",
                 _ or Normal => "Noto Sans CJK HK",
             };
@@ -65,10 +95,9 @@ partial class LinuxPlatformServiceImpl
             return fontWeightValue switch
             {
                 Thin => "Noto Sans CJK TC Thin",
-                ExtraLight => "Noto Sans CJK TC ExtraLight",
+                ExtraLight => "Noto Sans CJK TC DemiLight",
                 Light => "Noto Sans CJK TC Light",
                 Medium => "Noto Sans CJK TC Medium",
-                SemiBold => "Noto Sans CJK TC SemiBold",
                 Black => "Noto Sans CJK TC Black",
                 _ or Normal => "Noto Sans CJK TC",
             };
@@ -78,10 +107,9 @@ partial class LinuxPlatformServiceImpl
             return fontWeightValue switch
             {
                 Thin => "Noto Sans CJK JP Thin",
-                ExtraLight => "Noto Sans CJK JP ExtraLight",
+                ExtraLight => "Noto Sans CJK JP DemiLight",
                 Light => "Noto Sans CJK JP Light",
                 Medium => "Noto Sans CJK JP Medium",
-                SemiBold => "Noto Sans CJK JP SemiBold",
                 Black => "Noto Sans CJK JP Black",
                 _ or Normal => "Noto Sans CJK JP",
             };
@@ -91,10 +119,9 @@ partial class LinuxPlatformServiceImpl
             return fontWeightValue switch
             {
                 Thin => "Noto Sans CJK KR Thin",
-                ExtraLight => "Noto Sans CJK KR ExtraLight",
+                ExtraLight => "Noto Sans CJK KR DemiLight",
                 Light => "Noto Sans CJK KR Light",
                 Medium => "Noto Sans CJK KR Medium",
-                SemiBold => "Noto Sans CJK KR SemiBold",
                 Black => "Noto Sans CJK KR Black",
                 _ or Normal => "Noto Sans CJK KR",
             };
