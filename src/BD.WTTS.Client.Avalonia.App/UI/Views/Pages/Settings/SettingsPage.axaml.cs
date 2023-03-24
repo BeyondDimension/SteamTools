24c4136bd3c6ef156da1e62a744648dcb330d72d
RMBGAME <rmb@rmbgame.net>
2023-03-24 17:52:13 +0800

    settings

---
+++ src/BD.WTTS.Client.Avalonia.App/UI/Views/Pages/Settings/SettingsPage.axaml.cs
@@ -0,0 +1,15 @@
+using Avalonia.Controls;
+
+namespace BD.WTTS.UI.Views.Pages.Settings;
+public partial class UserControl1 : UserControl
+{
+    public UserControl1()
+    {
+        InitializeComponent();
+    }
+
+    void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
