748cf3aa910385f3bb2aedd51fd1d91dc5eec727
RMBGAME <rmb@rmbgame.net>
2023-03-27 00:54:53 +0800

    💄 UI

---
+++ src/BD.WTTS.Client.Avalonia.App/UI/Views/Controls/TitleBar.axaml.cs
@@ -0,0 +1,10 @@
+using Avalonia.Controls;
+
+namespace BD.WTTS.UI.Views.Controls;
+public partial class TitleBar : UserControl
+{
+    public TitleBar()
+    {
+        InitializeComponent();
+    }
+}
