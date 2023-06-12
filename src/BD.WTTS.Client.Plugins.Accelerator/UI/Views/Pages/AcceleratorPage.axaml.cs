cbd85541480bbd40a0b9bbc414cd9b2d0ae7b9e6
RMBGAME <rmb@rmbgame.net>
2023-06-12 08:52:47 +0800

    💄 Accelerator UI

--- src/BD.WTTS.Client.Plugins.Accelerator/UI/Views/Pages/AcceleratorPage.axaml.cs
+++ src/BD.WTTS.Client.Plugins.Accelerator/UI/Views/Pages/AcceleratorPage.axaml.cs
@@ -1,13 +1,16 @@
-using Avalonia.Controls;
-using Avalonia.ReactiveUI;
+using BD.WTTS.Client.Resources;
 
 namespace BD.WTTS.UI.Views.Pages;
 
-public partial class AcceleratorPage : ReactiveUserControl<AcceleratorPageViewModel>
+public partial class AcceleratorPage : PageBase<AcceleratorPageViewModel>
 {
     public AcceleratorPage()
     {
         InitializeComponent();
         DataContext = new AcceleratorPageViewModel();
+
+        Title = Strings.CommunityFix;
+        Subtitle = "插件作者: Steam++ 官方";
+        Description = "提供一些游戏相关网站服务的加速功能。";
     }
 }
