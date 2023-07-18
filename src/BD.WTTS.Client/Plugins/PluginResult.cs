e151743677288e82586dfe83cbac1b27dc92b8cb
Aigio Liu <lysoft.pw@foxmail.com>
2023-07-18 19:39:15 +0800

    🎨 PluginResult.PluginResult / ReactiveObject

--- src/BD.WTTS.Client/Plugins/PluginResult.cs
+++ src/BD.WTTS.Client/Plugins/PluginResult.cs
@@ -6,16 +6,28 @@ namespace BD.WTTS.Plugins;
 /// <typeparam name="TData">结果数据类型</typeparam>
 /// <param name="IsDisable">是否禁用</param>
 /// <param name="Data">插件数据</param>
-public readonly record struct PluginResult<TData>(bool IsDisable, TData Data)
-    : IEquatable<PluginResult<TData>>,
-    IEquatable<TData>
+public record PluginResult<TData>
+    : ReactiveRecord, IEquatable<PluginResult<TData>>,
+    IEquatable<TData>, IReactiveObject
     where TData : notnull
 {
-    public override readonly int GetHashCode() => Data.GetHashCode();
+    [SetsRequiredMembers]
+    public PluginResult(bool isDisable, TData data)
+    {
+        IsDisable = isDisable;
+        Data = data;
+    }
 
-    public readonly bool Equals(PluginResult<TData> other)
+    [property: Reactive]
+    public bool IsDisable { get; set; }
+
+    required public TData Data { get; init; }
+
+    public override int GetHashCode() => Data.GetHashCode();
+
+    bool IEquatable<PluginResult<TData>>.Equals(PluginResult<TData>? other)
         => EqualityComparer<TData>.Default.Equals(other.Data, Data);
 
-    public readonly bool Equals(TData? other)
+    public bool Equals(TData? other)
         => EqualityComparer<TData>.Default.Equals(other, Data);
 }
