33f780d6fb9a5329c382beba090bfcfa8a7b7378
ff m <Aa790210778@outlook.com>
2023-08-28 04:25:28 +0800

    修改 Windows Linux macOS 系统代理忽略 地址

--- src/BD.WTTS.Client/Services.Implementation/Platform/Linux/LinuxPlatformServiceImpl.SystemProxy.cs
+++ src/BD.WTTS.Client/Services.Implementation/Platform/Linux/LinuxPlatformServiceImpl.SystemProxy.cs
@@ -4,6 +4,8 @@ namespace BD.WTTS.Services.Implementation;
 
 partial class LinuxPlatformServiceImpl
 {
+
+    const string ProxyOverride = "['localhost', '127.*', '10.*', '172.16.*', '172.17.*', '172.18.*', '172.19.*', '172.20.*', '172.21.*', '172.22.*', '172.23.*', '172.24.*', '172.25.*', '172.26.*', '172.27.*', '172.28.*', '172.29.*', '172.30.*', '172.31.*', '192.168.*', '*.steampp.net']";
     public Task<bool> SetAsSystemProxyAsync(bool state, IPAddress? ip, int port)
     {
         var shellContent = new StringBuilder();
@@ -17,6 +19,7 @@ partial class LinuxPlatformServiceImpl
                 shellContent.AppendLine($"gsettings set org.gnome.system.proxy.http port {port}");
                 shellContent.AppendLine($"gsettings set org.gnome.system.proxy.https host '{ip}'");
                 shellContent.AppendLine($"gsettings set org.gnome.system.proxy.https port {port}");
+                shellContent.AppendLine($"gsettings set org.gnome.system.proxy ignore-hosts \"{ProxyOverride}\"");
             }
         }
         else
--- src/BD.WTTS.Client/Services.Implementation/Platform/MacCatalyst/MacCatalystPlatformServiceImpl.SystemProxy.cs
+++ src/BD.WTTS.Client/Services.Implementation/Platform/MacCatalyst/MacCatalystPlatformServiceImpl.SystemProxy.cs
@@ -4,6 +4,9 @@ namespace BD.WTTS.Services.Implementation;
 
 partial class MacCatalystPlatformServiceImpl
 {
+
+    const string ProxyOverride = "localhost 127.* 10.* 172.16.* 172.17.* 172.18.* 172.19.* 172.20.* 172.21.* 172.22.* 172.23.* 172.24.* 172.25.* 172.26.* 172.27.* 172.28.* 172.29.* 172.30.* 172.31.* 192.168.* *.steampp.net";
+    
     public Task<bool> SetAsSystemProxyAsync(bool state, IPAddress? ip, int port)
     {
 #if MACOS || MACCATALYST
@@ -23,6 +26,7 @@ partial class MacCatalystPlatformServiceImpl
                         shellContent.AppendLine($"networksetup -setwebproxystate '{item}' on");
                         shellContent.AppendLine($"networksetup -setsecurewebproxy '{item}' '{bindIP}' {port}");
                         shellContent.AppendLine($"networksetup -setsecurewebproxystate '{item}' on");
+                        shellContent.AppendLine($"networksetup -setproxybypassdomains '{item}' '{ProxyOverride}'");
                     }
                     else
                     {
