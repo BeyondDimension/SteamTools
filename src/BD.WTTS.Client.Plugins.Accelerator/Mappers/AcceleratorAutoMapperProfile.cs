25f6f108c65cbe02d7f52ce2ecf36b082ead44a2
ff m <Aa790210778@outlook.com>
2023-08-29 01:40:23 +0800

    🐛修复保存脚本状态 SaveScriptEnableAsync 中使用 !item.Disable Mapper 与旧 sqlite 反向映射

--- src/BD.WTTS.Client.Plugins.Accelerator/Mappers/AcceleratorAutoMapperProfile.cs
+++ src/BD.WTTS.Client.Plugins.Accelerator/Mappers/AcceleratorAutoMapperProfile.cs
@@ -10,12 +10,14 @@ sealed class AcceleratorAutoMapperProfile : Profile
             .ForMember(x => x.Author, d => d.MapFrom(x => x.AuthorName))
             .ForMember(x => x.Icon, d => d.MapFrom(x => x.IconUrl))
             .ForMember(x => x.Description, d => d.MapFrom(x => x.Describe))
+            .ForMember(x => x.Enable, d => d.MapFrom(x => !x.Disable))
             .ForMember(x => x.Id, d => d.MapFrom(x => x.LocalId));
         CreateMap<Script, ScriptDTO>()
             .ForMember(x => x.LocalId, d => d.MapFrom(x => x.Id))
             .ForMember(x => x.AuthorName, d => d.MapFrom(x => x.Author))
             .ForMember(x => x.Describe, d => d.MapFrom(x => x.Description))
             .ForMember(x => x.IconUrl, d => d.MapFrom(x => x.Icon))
+            .ForMember(x => x.Disable, d => d.MapFrom(x => !x.Enable))
             .ForMember(x => x.Id, d => d.MapFrom(x => x.Pid));
     }
 }
