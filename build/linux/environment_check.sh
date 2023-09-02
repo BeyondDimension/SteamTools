1c454123b894dee2e9ab9c72c2b24978accbabcd
ff m <Aa790210778@outlook.com>
2023-09-02 14:03:09 +0800

    ✨检查环境脚本 卸载安装脚本

---
+++ build/linux/environment_check.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+base_path="$HOME/WattToolkit"
+export LC_ALL=en_US.UTF-8
+
+Install_certutil() {
+    # 判断发行版类型
+    if command -v certutil &>/dev/null; then
+        echo "certutil 工具已安装。"
+    else
+        echo "证书导入以及验证需要使用 certutil 工具。"
+        # 判断包管理器
+        if command -v apt &>/dev/null; then
+            # 使用 apt (Debian/Ubuntu)
+            sudo apt update
+            sudo apt install -y libnss3-tools
+        elif command -v dnf &>/dev/null; then
+            # 使用 dnf (Fedora)
+            sudo dnf install -y nss-tools
+        elif command -v yum &>/dev/null; then
+            # 使用 yum (CentOS/Red Hat)
+            sudo yum install -y nss-tools
+        elif command -v pacman &>/dev/null; then
+            # 使用 pacman (Arch Linux)
+            # sudo pacman -S nss
+            echo "请手动安装 certutil 工具。"
+            exit 1
+        else
+            echo "请手动安装 certutil 工具。"
+            exit 1
+        fi
+        echo "certutil 工具已安装。"
+    fi
+}
+certutil_Init() {
+    certutil -d sql:$HOME/.pki/nssdb -L
+    ret=$?
+    if [ $ret -ne 0 ]; then
+        echo "即将初始化 certutil sql:\$HOME/.pki/nssdb"
+        mkdir -p sql:$HOME/.pki
+        mkdir -p sql:$HOME/.pki/nssdb
+        chmod 700 sql:$HOME/.pki/nssdb
+        certutil -d sql:$HOME/.pki/nssdb -N --empty-password
+    else
+        echo "certutil nssdb 正常"
+    fi
+}
+Install_certutil
+certutil_Init
---
+++ build/linux/uninstall.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+base_path="$HOME/WattToolkit"
+export LC_ALL=en_US.UTF-8
+exec_name="Steam++"
+app_name="Watt Toolkit"
+AppData="$XDG_DATA_HOME/Steam++"
+AppData_t="$HOME/.local/share/Steam++"
+Cache="$XDG_CACHE_HOME/Steam++"
+Cache_t="$HOME/.cache/Steam+"
+PROCESS_NAMES=("$exec_name" "$app_name")
+
+Kill_Process() {
+    # 尝试的次数
+    Kill_MAX_RETRIES=3
+
+    # 循环尝试终止进程
+    for process_name in "${PROCESS_NAMES[@]}"; do
+        kill_retry=1
+        while [ $kill_retry -le $Kill_MAX_RETRIES ]; do
+            pid=$(pgrep "$process_name")
+            if [ -n "$pid" ]; then
+                echo "尝试 $kill_retry: 进程 $process_name 正在运行中。正在终止..."
+                kill $pid
+                sleep 2
+            else
+                break
+            fi
+            kill_retry=$((kill_retry + 1))
+        done
+    done
+
+    for process_name in "${PROCESS_NAMES[@]}"; do
+        # 检查是否成功终止进程
+        if pgrep -x "$process_name" >/dev/null; then
+            echo "无法终止程序 $process_name。尝试次数已达上限。"
+            exit 1
+        else
+            echo "程序 $process_name 已成功终止。"
+        fi
+    done
+
+}
+Kill_Process
+
+zenity --question --text="是否删除用户数据" --width=400
+# 获取上一个命令的退出码
+response=$?
+
+if [ $response -eq 0 ]; then
+    rm -rfq "$AppData" 2>/dev/null
+    rm -rfq "$AppData_t" 2>/dev/null
+else
+    echo "保留用户数据方便下次安装。"
+    echo "如需删除可手动删除该目录:$AppData 或者 $AppData_t"
+fi
+rm -rfq "$Cache" 2>/dev/null
+rm -rfq "$Cache_t" 2>/dev/null
+rm -rfq "$base_path" 2>/dev/null
+rm -rfq "$HOME/Desktop/Watt Toolkit.desktop" 2>/dev/null
+zenity --info --text="卸载完成!" --width=300
