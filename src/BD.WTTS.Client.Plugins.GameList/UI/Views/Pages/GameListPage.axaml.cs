34341172b87e8ac9de4a5112005f55631e86b500
RMBGAME <rmb@rmbgame.net>
2024-05-29 19:15:59 +0800

    🚧 ItemsRepeater

--- src/BD.WTTS.Client.Plugins.GameList/UI/Views/Pages/GameListPage.axaml.cs
+++ src/BD.WTTS.Client.Plugins.GameList/UI/Views/Pages/GameListPage.axaml.cs
@@ -11,44 +11,7 @@ public partial class GameListPage : PageBase<GameListPageViewModel>
     {
         InitializeComponent();
         DataContext ??= new GameListPageViewModel();
-
-        //GameLibrarySettings.GameLibraryLayoutType.Subscribe(x =>
-        //{
-        //    GameListRepeater.Layout = GetGameLayout(x);
-        //});
     }
-
-    //static VirtualizingLayout GetGameLayout(GridLayoutType layoutType)
-    //{
-    //    VirtualizingLayout layout = layoutType switch
-    //    {
-    //        GridLayoutType.List => new StackLayout()
-    //        {
-    //            Orientation = Orientation.Vertical,
-    //            Spacing = 10,
-    //        },
-    //        GridLayoutType.CompactGrid => new UniformGridLayout()
-    //        {
-    //            MinItemWidth = 230,
-    //            MinItemHeight = 65,
-    //            MinColumnSpacing = 5,
-    //            MinRowSpacing = 5,
-    //            ItemsJustification = UniformGridLayoutItemsJustification.Start,
-    //            ItemsStretch = UniformGridLayoutItemsStretch.Uniform,
-    //        },
-    //        _ => new UniformGridLayout()
-    //        {
-    //            MinItemWidth = 150,
-    //            MinItemHeight = 310,
-    //            MinColumnSpacing = 10,
-    //            MinRowSpacing = 10,
-    //            ItemsJustification = UniformGridLayoutItemsJustification.Start,
-    //            ItemsStretch = UniformGridLayoutItemsStretch.Uniform,
-    //        },
-    //    };
-
-    //    return layout;
-    //}
 
     private void AppOpenLink_MenuItem_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
     {
