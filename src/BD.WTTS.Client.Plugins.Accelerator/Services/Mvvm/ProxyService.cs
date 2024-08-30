83c390e4467259d709af529f634a104597e348bb
k1mlka <k1mlka@outlook.com>
2024-08-30 19:20:15 +0800

    🐛 通过脚本开关来确定是否显示脚本配置页面

--- src/BD.WTTS.Client.Plugins.Accelerator/Services/Mvvm/ProxyService.cs
+++ src/BD.WTTS.Client.Plugins.Accelerator/Services/Mvvm/ProxyService.cs
@@ -152,7 +152,7 @@ public sealed partial class ProxyService
 
             ProxyStarting = false;
             ProxyStatus = startOrStop;
-            IsAnyProxyScripts = ProxyStatus && CheckProxyScriptsEnable();
+            IsAnyProxyScripts = ProxyStatus && IsEnableScript;
         }
     }
 
